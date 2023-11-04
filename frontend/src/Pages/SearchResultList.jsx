9d28e3689b3fe8cbdcf2c14b9b3b692512d9dbf2
Areeb <muhammadareeb334@gmail.com>
2023-11-04 23:06:06 +0500

    commit the changes

---
+++ frontend/src/Pages/SearchResultList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchResultList = () => {
+  return (
+    <div>
+      SearchReasultList
+    </div>
+  )
+}
+
+export default SearchResultList
---
+++ frontend/src/Router/Routers.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./../Pages/Home";
+import Login from "./../Pages/Login";
+import Register from "./../Pages/Register";
+import SearchResultList from "./../Pages/SearchResultList";
+import TourDetail from "./../Pages/TourDetail";
+import Tours from "./../Pages/Tours";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/tours" element={<Tours />} />
+      <Route path="/tours:id" element={<TourDetail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/tours/search" element={<SearchResultList />} />
+    </Routes>
+  );
+};
+
+export default Routers;
