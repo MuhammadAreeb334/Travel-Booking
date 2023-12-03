764cc677f137b8ff5d7974a5a14f0072a1fb2dce
Areeb <muhammadareeb334@gmail.com>
2023-12-03 14:30:30 +0500

    commit all changes

--- frontend/src/Pages/Tours.jsx
+++ frontend/src/Pages/Tours.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import CommonSection from '../Shared/CommonSection'
+import '../Styles/tour.css'
+import tourData from '../assets/data/tours'
+import TourCard from '../Shared/TourCard'
+import SearchBar from '../Shared/SearchBar'
+import NewsLetter from '../Shared/Newslatter'
+import { Container, Row } from 'reactstrap'
 
 const Tours = () => {
   return (
-    <div>
-      Tours
-    </div>
+    <>
+      <CommonSection title={'"Discover Your Adventure: All Tours"'} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+    </>
   )
 }
 
